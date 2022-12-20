b0292ff3a11c641d54fe9e89918970a30227c6c6
Bart Massey <bart.massey@gmail.com>
2022-12-20 00:41:20 -0800

    added more tests and an example

---
+++ examples/gain.rs
@@ -0,0 +1,31 @@
+use librice::{*, bitstream_io::*};
+
+fn main() {
+    use std::io::Cursor;
+    use rand::*;
+
+    let mut rng = rand::thread_rng();
+
+    for k in 1..13 {
+        let msg: [u16; 32] = std::array::from_fn(|_| rng.gen_range(0..(1 << (k + 3))));
+
+        let codec = Codec(k);
+        let c = Cursor::new(Vec::new());
+
+        let mut b: BitWriter<_, BE> = BitWriter::new(c);
+        for w in msg {
+            codec.encode_word(w, &mut b).unwrap();
+        }
+        b.write::<u32>(16, 0).unwrap();
+
+        let mut c = b.into_writer();
+        println!("k = {k}, ratio = {}", c.get_ref().len() as f64 / 64.0);
+        c.set_position(0);
+
+        let mut r: BitReader<_, BE> = BitReader::new(c);
+        let dmsg: Vec<u16> = (0..32)
+            .map(|_| codec.decode_word(&mut r).unwrap())
+            .collect();
+        assert_eq!(msg.as_ref(), &dmsg);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
+pub use bitstream_io;
 use bitstream_io::*;
 
-pub struct Codec(u32);
+pub struct Codec(pub u32);
 
 impl Codec {
     pub fn encode_word<T, W: BitWrite>(&self, src: T, w: &mut W) -> std::io::Result<()>
@@ -71,5 +72,34 @@ fn test_roundtrip() {
         let j = codec.decode_word(&mut b).unwrap();
 
         assert_eq!(i, j);
+    }
+}
+
+#[test]
+fn test_random() {
+    use std::io::Cursor;
+    use rand::*;
+
+    let mut rng = rand::thread_rng();
+    let msg: [u16; 32] = std::array::from_fn(|_| rng.gen());
+
+    for k in 1..13 {
+        let codec = Codec(k);
+        let c = Cursor::new(Vec::new());
+
+        let mut b: BitWriter<_, BE> = BitWriter::new(c);
+        for w in msg {
+            codec.encode_word(w, &mut b).unwrap();
+        }
+        b.write::<u32>(16, 0).unwrap();
+
+        let mut c = b.into_writer();
+        c.set_position(0);
+
+        let mut r: BitReader<_, BE> = BitReader::new(c);
+        let dmsg: Vec<u16> = (0..32)
+            .map(|_| codec.decode_word(&mut r).unwrap())
+            .collect();
+        assert_eq!(msg.as_ref(), &dmsg);
     }
 }
